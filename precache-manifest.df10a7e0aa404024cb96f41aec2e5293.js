9b421314b3f0a6e090c87f44bcb0f0620f0330c6
nuredin10 <nuredinibrahim18@gmail.com>
2022-02-04 13:51:15 +0300

    Updates

---
+++ precache-manifest.df10a7e0aa404024cb96f41aec2e5293.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253dd3d4199fc01c1c39d15c9c1c628e",
+    "url": "/Explore-Ethiopia/index.html"
+  },
+  {
+    "revision": "80140c1f7504c63e2b13",
+    "url": "/Explore-Ethiopia/static/css/main.7794bdac.chunk.css"
+  },
+  {
+    "revision": "6fd874e471c08b1d2dc7",
+    "url": "/Explore-Ethiopia/static/js/2.7401a318.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Explore-Ethiopia/static/js/2.7401a318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80140c1f7504c63e2b13",
+    "url": "/Explore-Ethiopia/static/js/main.92600180.chunk.js"
+  },
+  {
+    "revision": "9cbc864931391bd132c2",
+    "url": "/Explore-Ethiopia/static/js/runtime-main.9212e0b5.js"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/Explore-Ethiopia/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "32f858387a68b98c35ded2b4dfb16fc8",
+    "url": "/Explore-Ethiopia/static/media/img-2.32f85838.jpg"
+  },
+  {
+    "revision": "d361091b64a71a4fddba3173107e969d",
+    "url": "/Explore-Ethiopia/static/media/img-3.d361091b.jpg"
+  },
+  {
+    "revision": "873141ee10c4105e7228eb3d5b46f8b7",
+    "url": "/Explore-Ethiopia/static/media/img-4.873141ee.jpg"
+  },
+  {
+    "revision": "005b2225250551ad809f56288d2361dc",
+    "url": "/Explore-Ethiopia/static/media/img-8.005b2225.jpg"
+  },
+  {
+    "revision": "6ce2be0ce9b87fdc3e8280ff1c48889f",
+    "url": "/Explore-Ethiopia/static/media/img-9.6ce2be0c.jpg"
+  },
+  {
+    "revision": "07218d3d2113a64a67b0e009df80a539",
+    "url": "/Explore-Ethiopia/static/media/img-home.07218d3d.jpg"
+  }
+]);
